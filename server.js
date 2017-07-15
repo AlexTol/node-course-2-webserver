09294477827b8f5209cccf64e722edc19d276376
Alex Tol <atol514@gmail.com>
2017-07-14 17:58:28 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+//lets hbs know where we're going to keep our partials
+hbs.registerPartials(__dirname + '/views/partials');
+//we tell express that we want to use hbs as the view engine
+app.set('view engine', 'hbs');
+
+//'next' exists to tell express when the middleware function is done
+app.use((req,res,next) => {
+  var now = new Date().toString();
+
+  var log = `${now}: ${req.method} ${req.url}`;
+  fs.appendFile('server.log',log + '\n', (err) => {
+
+      if(err)
+      {
+        console.log('unable to log into server.log');
+      }
+  });
+    console.log(log);
+  next();//we must call next or else the other methods will not be able to be called;
+});
+
+/**
+//this method stop everything below from executing
+app.use((req,res,next) => {
+
+  res.render('maintenance.hbs',{
+    pageTitle: 'Maintenance',
+    welcomeMessage: 'Sorry!, our page is currently undergoing maintenance'
+  });
+
+});
+*/
+
+//__dirname stores the path of your projects directory
+  //express.static directly serves static files from whatever directory is passed in
+app.use(express.static(__dirname + '/public'));
+
+//helpers are methods that are called upon all hbs templates to return some dynamic data
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+//req stores information about the request coming in
+//res has a bunch of mehtods available to respond to the request
+app.get('/', (req,res) =>{
+  //send html
+  //res.send('<h1>Hello Express</h1>');
+
+  //send object in form of JSON
+  /**res.send({
+    name: 'Alex',
+    likes: ['cats','poached eggs','memes']
+    });    */
+
+
+    res.render('home.hbs',{
+      pageTitle: 'Home',
+      currentYear: new Date().getFullYear(),
+      welcomeMessage: 'Welcome, Valued User!'
+    });
+});
+
+app.get('/about', (req,res) => {
+  res.render('about.hbs',{
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/bad', (req,res) => {
+  res.send({
+    errorMessage: 'bad request!'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('server is up on port 3000');
+});
